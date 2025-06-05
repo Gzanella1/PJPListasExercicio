e4fe8f11e146ce035c2d769904d9783cbb498940
GIOVANI ZANELA DA MAIA <giovani.maia@alunos.sc.senac.br>
2025-06-05 11:25:47 -0300

    poo

--- src/listaPOO/listaPoo_2/Aluno.java
+++ src/listaPOO/listaPoo_2/Aluno.java
@@ -1,5 +1,12 @@
 package listaPOO.listaPoo_2;
 
 public class Aluno {
-
+	String nome;
+	String matricula;
+	String curso;
+	String[] disciplinas = new String[3];
+	double[] notas = new double[3];
+	
+	
+	
 }
